bd86f59ef18fbb078b018cfe8e75558b998176b6
hakanozoglu <hakan.ozoglu@grapecity.com>
2022-03-21 15:53:55 +0300

    fix seeder

--- src/model/seeder.ts
+++ src/model/seeder.ts
@@ -1,13 +1,12 @@
 import { Label } from "./Label";
 
-Label.destroy({truncate:{ cascade: false }});
-Label.create({
-  id: 1,
-  title: "Önemli",
-  color: "red",
-});
-Label.create({
-  id: 2,
-  title: "Önemsiz",
-  color: "gray",
-});
+Label.findOrCreate({
+  where:{id:1},defaults:{
+    id:1,title:"Önemli",color:"red"
+  }
+})
+Label.findOrCreate({
+  where:{id:2},defaults:{
+    id:2,title:"Önemsiz",color:"gray"
+  }
+})
