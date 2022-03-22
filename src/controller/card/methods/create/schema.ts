a3459c82553d299e8b1d309d8435f5adb87b7cb3
hakanozoglu <hakan.ozoglu@grapecity.com>
2022-03-22 20:09:26 +0300

    zod transform date function

--- src/controller/card/methods/create/schema.ts
+++ src/controller/card/methods/create/schema.ts
@@ -4,6 +4,6 @@ export const body = zod.object({
   title: zod.string(),
   listId: zod.number(),
   description: zod.string().optional(),
-  duedate: zod.string().regex(/^\d{4}-([0]\d|1[0-2])-([0-2]\d|3[01])$/).optional(),
+  duedate: zod.string().regex(/^\d{4}-([0]\d|1[0-2])-([0-2]\d|3[01])$/).transform(Date).optional(),
   order:zod.number().optional()
 });
